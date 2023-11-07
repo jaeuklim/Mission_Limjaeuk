831f2024e2e77ddae03f37da8f943d8c4a98bc40
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-07 11:16:50 +0900

    1단계  수행

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,22 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+
+        Scanner  scanner= new Scanner(System.in);
+
+        while(true){
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            if(cmd.equals("종료"))
+                return;
+        }
+
+
     }
 }
+
