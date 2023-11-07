76970ee52b34a21deec190541a1a8aff1293704b
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-07 16:35:26 +0900

    5단계 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -28,6 +28,15 @@ public class Main {
                 list.add(new Myeongeon(num,myeong,author));
                 System.out.println(num + "번 명언이 등록되었습니다.");
                 num++;
+            }
+
+            if(cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for(int i = list.size()-1; i>=0; i--)
+                    list.get(i).get();
+
 
             }
         }
@@ -37,12 +46,33 @@ public class Main {
 
 class Myeongeon {
     int num;
-    String Myeong;
+    String myeong;
     String author;
 
     Myeongeon(int num, String myeong, String author){
         this.num = num;
-        this.Myeong = myeong;
+        this.myeong = myeong;
         this.author = author;
     }
+
+    void get(){
+        System.out.println(num + " / " + myeong + " / " + author);
+    }
 }
+//
+//목록
+//        == 명언 앱 ==
+//        명령) 등록
+//        명언 : 현재를 사랑하라.
+//        작가 : 작자미상
+//        1번 명언이 등록되었습니다.
+//        명령) 등록
+//        명언 : 현재를 사랑하라.
+//        작가 : 작자미상
+//        2번 명언이 등록되었습니다.
+//        명령) 목록
+//        번호 / 작가 / 명언
+//        ----------------------
+//        2 / 작자미상 / 과거에 집착하지 마라.
+//        1 / 작자미상 / 현재를 사랑하라.
+//        명령) 종료
